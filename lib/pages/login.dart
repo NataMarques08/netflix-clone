2ffadc3203b403ef98fb242621c1b4cdfd666a2f
NataMarques08 <nmarques0897@gmail.com>
2023-08-21 21:48:41 -0300

    Netflix clone login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -25,92 +25,95 @@ class _LoginState extends State<Login> {
             Container(
               color: Colors.black.withOpacity(0.9),
             ),
-            Column(
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 200),
-                  child: Image.asset('assets/netflix-logo.png'),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                emailInput(),
-                passwordInput(),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 8.0, left: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Forgot your email and password?',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 80,
                   ),
-                ),
-                const SizedBox(
-                  height: 80.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0, right: 30),
-                  child: Positioned(
-                    bottom: 20,
-                    right: 20,
-                    left: 20,
-                    child: Container(
-                      height: 50,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromARGB(255, 243, 91, 91),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              margin: const EdgeInsets.only(right: 40.0),
-                              color: const Color.fromARGB(255, 255, 17, 0),
-                              child: const Center(
-                                child: Text(
-                                  'Sign in',
-                                  textScaleFactor: 1.0,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 200),
+                    child: Image.asset('assets/netflix-logo.png'),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  emailInput(),
+                  passwordInput(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8.0, left: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Forgot your email and password?',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 120.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 30.0, right: 30, bottom: 45),
+                    child: Positioned(
+                      bottom: 0,
+                      right: 0,
+                      left: 0,
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(255, 243, 91, 91),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 40.0),
+                                color: const Color.fromARGB(255, 255, 17, 0),
+                                child: const Center(
+                                  child: Text(
+                                    'Sign in',
+                                    textScaleFactor: 1.0,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            child: Container(
-                              margin: EdgeInsets.only(right: 40.0),
-                              child: const Center(
-                                child: Text(
-                                  'Login with facebook',
-                                  textScaleFactor: 1.0,
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold),
+                            Expanded(
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 40.0),
+                                child: const Center(
+                                  child: Text(
+                                    'Login with facebook',
+                                    textScaleFactor: 1.0,
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
@@ -118,9 +121,8 @@ class _LoginState extends State<Login> {
     );
   }
 
-  Expanded passwordInput() {
-    return const Expanded(
-        child: Padding(
+  Padding passwordInput() {
+    return const Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
         children: [
@@ -134,12 +136,11 @@ class _LoginState extends State<Login> {
           ),
         ],
       ),
-    ));
+    );
   }
 
-  Expanded emailInput() {
-    return const Expanded(
-        child: Padding(
+  Padding emailInput() {
+    return const Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
         children: [
@@ -153,6 +154,6 @@ class _LoginState extends State<Login> {
           ),
         ],
       ),
-    ));
+    );
   }
 }
--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -22,180 +22,182 @@ class StartPage extends StatelessWidget {
             Container(
               color: Colors.black.withOpacity(0.9),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 80,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 200),
-                        child: Image.asset(
-                          'assets/netflix-logo.png',
-                          width: 148,
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 80,
                         ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 200),
+                          child: Image.asset(
+                            'assets/netflix-logo.png',
+                            width: 148,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        const Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(right: 140.0),
+                                        child: Text(
+                                          'WATCH',
+                                          style: TextStyle(
+                                            fontSize: 58,
+                                            fontWeight: FontWeight.w900,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        'TV SHOWS &',
+                                        style: TextStyle(
+                                          fontSize: 58,
+                                          fontWeight: FontWeight.w900,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(right: 120.0),
+                                        child: Text(
+                                          'MOVIES',
+                                          style: TextStyle(
+                                            fontSize: 58,
+                                            fontWeight: FontWeight.w900,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 30.0),
+                                  child: Text(
+                                    'ANYWHERE.',
+                                    style: TextStyle(
+                                      fontSize: 58,
+                                      fontWeight: FontWeight.w900,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 30.0),
+                                  child: Text(
+                                    'ANYTIME.',
+                                    style: TextStyle(
+                                      fontSize: 58,
+                                      fontWeight: FontWeight.w900,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Color.fromARGB(255, 243, 91, 91))
+                        ],
                       ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      const Column(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Padding(
-                                      padding: EdgeInsets.only(right: 140.0),
-                                      child: Text(
-                                        'WATCH',
-                                        style: TextStyle(
-                                          fontSize: 58,
-                                          fontWeight: FontWeight.w900,
-                                          color: Colors.white,
-                                        ),
-                                      ),
+                          Expanded(
+                            child: Center(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => const Login(),
                                     ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      'TV SHOWS &',
-                                      style: TextStyle(
-                                        fontSize: 58,
-                                        fontWeight: FontWeight.w900,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Padding(
-                                      padding: EdgeInsets.only(right: 120.0),
-                                      child: Text(
-                                        'MOVIES',
-                                        style: TextStyle(
-                                          fontSize: 58,
-                                          fontWeight: FontWeight.w900,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(left: 30.0),
-                                child: Text(
-                                  'ANYWHERE.',
+                                  );
+                                },
+                                child: const Text(
+                                  'Login',
+                                  textScaleFactor: 1.0,
                                   style: TextStyle(
-                                    fontSize: 58,
-                                    fontWeight: FontWeight.w900,
-                                    color: Colors.white,
-                                  ),
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ),
-                            ],
+                            ),
                           ),
-                          Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(left: 30.0),
+                          Expanded(
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 255, 17, 0),
+                              ),
+                              child: const Center(
                                 child: Text(
-                                  'ANYTIME.',
+                                  'Start Free Month',
+                                  textScaleFactor: 1.0,
                                   style: TextStyle(
-                                    fontSize: 58,
-                                    fontWeight: FontWeight.w900,
-                                    color: Colors.white,
-                                  ),
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ),
-                            ],
+                            ),
                           ),
                         ],
-                      )
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  left: 0,
-                  child: Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(color: Color.fromARGB(255, 243, 91, 91))
-                      ],
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Expanded(
-                          child: Center(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => const Login(),
-                                  ),
-                                );
-                              },
-                              child: const Text(
-                                'Login',
-                                textScaleFactor: 1.0,
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Color.fromARGB(255, 255, 17, 0),
-                            ),
-                            child: const Center(
-                              child: Text(
-                                'Start Free Month',
-                                textScaleFactor: 1.0,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
